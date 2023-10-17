333bb235c86148553d84a9b02654fcf67d261bf0
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2023-10-17 12:25:25 +0200

    UPDATE: tests

--- src/problemSet1.py
+++ src/problemSet1.py
@@ -43,6 +43,9 @@
 #  [1,0] - down
 #  [-1,0] - up
 
+import unittest
+
+
 def localize(colors, measurements, motions, sensor_right, p_move):
     # initializes p to a uniform distribution over a grid of the same dimensions as colors
     pinit = 1.0 / float(len(colors)) / float(len(colors[0]))
@@ -75,3 +78,128 @@ measurements = ['G', 'G', 'G', 'G', 'G']
 motions = [[0, 0], [0, 1], [1, 0], [1, 0], [0, 1]]
 p = localize(colors, measurements, motions, sensor_right=0.7, p_move=0.8)
 show(p)  # displays your answer
+
+
+class TestLocalize(unittest.TestCase):
+    def test_1(self):
+        colors = [['G', 'G', 'G'],
+                  ['G', 'R', 'G'],
+                  ['G', 'G', 'G']]
+        measurements = ['R']
+        motions = [[0, 0]]
+        sensor_right = 1.0
+        p_move = 1.0
+        p = localize(colors, measurements, motions, sensor_right, p_move)
+        correct_answer = (
+            [[0.0, 0.0, 0.0],
+             [0.0, 1.0, 0.0],
+             [0.0, 0.0, 0.0]])
+        self.assertEqual(p, correct_answer)
+
+    def test_2(self):
+        colors = [['G', 'G', 'G'],
+                  ['G', 'R', 'R'],
+                  ['G', 'G', 'G']]
+        measurements = ['R']
+        motions = [[0, 0]]
+        sensor_right = 1.0
+        p_move = 1.0
+        p = localize(colors, measurements, motions, sensor_right, p_move)
+        correct_answer = (
+            [[0.0, 0.0, 0.0],
+             [0.0, 0.5, 0.5],
+             [0.0, 0.0, 0.0]])
+        self.assertEqual(p, correct_answer)
+
+    def test_3(self):
+        colors = [['G', 'G', 'G'],
+                  ['G', 'R', 'R'],
+                  ['G', 'G', 'G']]
+        measurements = ['R']
+        motions = [[0, 0]]
+        sensor_right = 0.8
+        p_move = 1.0
+        p = localize(colors, measurements, motions, sensor_right, p_move)
+        correct_answer = (
+            [[0.06666666666, 0.06666666666, 0.06666666666],
+             [0.06666666666, 0.26666666666, 0.26666666666],
+             [0.06666666666, 0.06666666666, 0.06666666666]])
+        self.assertEqual(p, correct_answer)
+
+    def test_4(self):
+        colors = [['G', 'G', 'G'],
+                  ['G', 'R', 'R'],
+                  ['G', 'G', 'G']]
+        measurements = ['R', 'R']
+        motions = [[0, 0], [0, 1]]
+        sensor_right = 0.8
+        p_move = 1.0
+        p = localize(colors, measurements, motions, sensor_right, p_move)
+        correct_answer = (
+            [[0.03333333333, 0.03333333333, 0.03333333333],
+             [0.13333333333, 0.13333333333, 0.53333333333],
+             [0.03333333333, 0.03333333333, 0.03333333333]])
+        self.assertEqual(p, correct_answer)
+
+    def test_5(self):
+        colors = [['G', 'G', 'G'],
+                  ['G', 'R', 'R'],
+                  ['G', 'G', 'G']]
+        measurements = ['R', 'R']
+        motions = [[0, 0], [0, 1]]
+        sensor_right = 1.0
+        p_move = 1.0
+        p = localize(colors, measurements, motions, sensor_right, p_move)
+        correct_answer = (
+            [[0.0, 0.0, 0.0],
+             [0.0, 0.0, 1.0],
+             [0.0, 0.0, 0.0]])
+        self.assertEqual(p, correct_answer)
+
+    def test_6(self):
+        colors = [['G', 'G', 'G'],
+                  ['G', 'R', 'R'],
+                  ['G', 'G', 'G']]
+        measurements = ['R', 'R']
+        motions = [[0, 0], [0, 1]]
+        sensor_right = 0.8
+        p_move = 0.5
+        p = localize(colors, measurements, motions, sensor_right, p_move)
+        correct_answer = (
+            [[0.0289855072, 0.0289855072, 0.0289855072],
+             [0.0724637681, 0.2898550724, 0.4637681159],
+             [0.0289855072, 0.0289855072, 0.0289855072]])
+
+    def test_7(self):
+        colors = [['G', 'G', 'G'],
+                  ['G', 'R', 'R'],
+                  ['G', 'G', 'G']]
+        measurements = ['R', 'R']
+        motions = [[0, 0], [0, 1]]
+        sensor_right = 1.0
+        p_move = 0.5
+        p = localize(colors, measurements, motions, sensor_right, p_move)
+        correct_answer = (
+            [[0.0, 0.0, 0.0],
+             [0.0, 0.33333333, 0.66666666],
+             [0.0, 0.0, 0.0]])
+        self.assertEqual(p, correct_answer)
+
+    def test_8(self):
+        colors = [['R', 'G', 'G', 'R', 'R'],
+                  ['R', 'R', 'G', 'R', 'R'],
+                  ['R', 'R', 'G', 'G', 'R'],
+                  ['R', 'R', 'R', 'R', 'R']]
+        measurements = ['G', 'G', 'G', 'G', 'G']
+        motions = [[0, 0], [0, 1], [1, 0], [1, 0], [0, 1]]
+        p = localize(colors, measurements, motions,
+                     sensor_right=0.7, p_move=0.8)
+        correct_answer = [[0.01105, 0.02464, 0.06799, 0.04472, 0.02465],
+                          [0.00715, 0.01017, 0.08696, 0.07988, 0.00935],
+                          [0.00739, 0.00894, 0.11272, 0.35350, 0.04065],
+                          [0.00910, 0.00715, 0.01434, 0.04313, 0.03642]]
+        self.assertEqual(p, correct_answer)
+
+
+if __name__ == "__main__":
+    unittest.main()
