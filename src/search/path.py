6d026d802d2cdbb11a4e0d60c1c96029b556fb94
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2023-10-22 11:22:42 +0200

    UPDATE: print path

---
+++ src/search/path.py
@@ -0,0 +1,95 @@
+# -----------
+# User Instructions:
+#
+# Modify the the search function so that it returns
+# a shortest path as follows:
+#
+# [['>', 'v', ' ', ' ', ' ', ' '],
+#  [' ', '>', '>', '>', '>', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', '*']]
+#
+# Where '>', '<', '^', and 'v' refer to right, left,
+# up, and down motions. Note that the 'v' should be
+# lowercase. '*' should mark the goal cell.
+#
+# You may assume that all test cases for this function
+# will have a path from init to goal.
+# ----------
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0],  # go up
+         [0, -1],  # go left
+         [1, 0],  # go down
+         [0, 1]]  # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+
+def isGoal(goal, x, y):
+    return goal[0] == x and goal[1] == y
+
+
+def isValidCell(grid, closed, x, y):
+    if (x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0])):
+        return False
+    return closed[x][y] == [-1, -1] and grid[x][y] == 0
+
+
+def getChar(current, previous):
+    x1, y1 = current
+    x2, y2 = previous
+    diff = [x1 - x2, y1 - y2]
+    for comp, char in zip(delta, delta_name):
+        if (diff == comp):
+            return char
+    raise ValueError("Cells not contiguos")
+
+
+def makePath(grid, init, goal, closed):
+    path = [[' ' for _ in col] for col in grid]
+    path[goal[0]][goal[1]] = '*'
+    current = goal
+    previous = closed[goal[0]][goal[1]]
+    while (current != init):
+        path[previous[0]][previous[1]] = getChar(current, previous)
+        current = previous
+        previous = closed[current[0]][current[1]]
+    return path
+
+
+def search(grid, init, goal, cost):
+    # ----------------------------------------
+    # modify code below
+    # ----------------------------------------
+    searching = [[0, init[0], init[1]]]
+    closed = [[[-1, -1] for _ in col] for col in grid]
+    closed[init[0]][init[1]] = init
+    while searching:
+        nextUp = min(searching)
+        searching.remove(nextUp)
+        g, x, y = nextUp
+        if (isGoal(goal, x, y)):
+            break
+        for dlt in delta:
+            x2 = x + dlt[0]
+            y2 = y + dlt[1]
+            if (isValidCell(grid, closed, x2, y2)):
+                searching.append([g + cost, x2, y2])
+                closed[x2][y2] = [x, y]
+    return makePath(grid, init, goal, closed)
+
+
+if __name__ == "__main__":
+    expand = search(grid, init, goal, cost)
+    for col in expand:
+        print(col)
